bc58433c4ae2b1e16cbeced87b55854458099492
Ismailduman <issmailduman@gmail.com>
2023-08-06 21:55:58 +0200

    @db tag

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -9,10 +9,10 @@ import org.junit.runner.RunWith;
 
         plugin = {
                 "html:target/cucumber-report.html",
-                "rerun:target/rerun.txt" ,
+                "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
-        features = "src/test/resources/features" ,
+        features = "src/test/resources/features",
         glue = "com/library/steps",
         dryRun = false,
         tags = "@wip"
